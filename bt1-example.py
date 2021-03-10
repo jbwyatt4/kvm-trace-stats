03921a341951dcae163711b2ee93428514f19e88
John B. Wyatt IV <jbwyatt4@gmail.com>
2021-03-10 00:30:51 -0800

    try fix for bt1

--- bt1-example.py
+++ bt1-example.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-
-try:
-    import babeltrace
-except ImportError:
-    # quick fix for debian-based distros
-    sys.path.append("/usr/local/lib/python%d.%d/site-packages" %
-                    (sys.version_info.major, sys.version_info.minor))
-    import babeltrace
+import babeltrace
 
 trace_path = sys.argv[1]
 
