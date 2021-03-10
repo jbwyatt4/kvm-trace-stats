f880ccc1e6ee5e33fa3306916c43fcd217119c4b
John B. Wyatt IV <jbwyatt4@gmail.com>
2021-03-09 23:34:29 -0800

    add bt1 example

---
+++ bt1-example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import babeltrace.reader
+import sys
+
+trace_path = sys.argv[1]
+
+trace_collection = babeltrace.reader.TraceCollection()
+
+trace_collection.add_trace(trace_path, 'ctf')
+
+for event in trace_collection.events:
+  print(event.name)
