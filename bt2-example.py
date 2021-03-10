88e6f2746538defa24a65649058d8ac5a857d45e
John B. Wyatt IV <jbwyatt4@gmail.com>
2021-03-09 23:15:10 -0800

    add bt2 error

--- bt2-example.py
+++ bt2-example.py
@@ -2,6 +2,8 @@
 
 import bt2
 
+# RuntimeError: Some auto source component specs did not produce any component: ~/perf.data
+
 for msg in bt2.TraceCollectionMessageIterator('~/ctf'):
   if type(msg) is bt2._EventMessageConst:
     print(msg.event.name)
